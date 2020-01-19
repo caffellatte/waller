04856d12df2b28dbf557d07da398cd817a7ac681
caffellatte <m.g.lutsenko@gmail.com>
2020-01-19 07:08:37 +0300

    Fix `npm run lint` errors.

--- renderer-process/index.js
+++ renderer-process/index.js
@@ -8,7 +8,7 @@ menu.append(new MenuItem({
   label: 'VK',
   click: () => {
     // Trigger an alert when menu item is clicked
-    alert('Deleted');
+    console.log('Deleted');
   }
 }));
 
@@ -16,16 +16,14 @@ menu.append(new MenuItem({
   label: 'More Info...',
   click: () => {
     // Trigger an alert when menu item is clicked
-    alert('Here is more information');
+    console.log('Here is more information');
   }
 }));
 
 // Add the listener
 document.addEventListener('DOMContentLoaded', () => {
-
-  let filesContext = document.querySelectorAll('.file_arq');
-
-  filesContext.forEach(function(el){
+  const filesContext = document.querySelectorAll('.file_arq');
+  filesContext.forEach(el => {
     el.addEventListener('click', event => {
       event.preventDefault();
       menu.popup(remote.getCurrentWindow());
